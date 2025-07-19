e1b677992cd1661f6c281794af6258a144c00c80
IlyaStepaniuk <ilya.stepaniuk@patrianna.com>
2025-07-19 19:48:35 +0300

    add vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { poppins } from '@/styles/fonts'
 import '@/styles/globals.scss'
 import { GoogleAnalytics } from '@/components/GoogleAnalytics'
 import { GOOGLE_TAG_MANAGER_ID, isAnalyticsEnabled } from '@/constants/analytics'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className={poppins.variable}>
         {isAnalyticsEnabled() && GOOGLE_TAG_MANAGER_ID && <GoogleAnalytics gtmId={GOOGLE_TAG_MANAGER_ID} />}
+        <SpeedInsights />
         {children}
       </body>
     </html>
